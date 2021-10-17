994ffdb7d86e2f81d1858b475926d1ac1aa6f68a
toshinori.chiba <toshinori.chiba.p@gmail.com>
2021-10-18 08:06:00 +0900

    戻し

--- api-server/src/main/scala/com/github/rinotc/bank/apiServer/Main.scala
+++ api-server/src/main/scala/com/github/rinotc/bank/apiServer/Main.scala
@@ -1,7 +1,19 @@
 package com.github.rinotc.bank.apiServer
 
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import com.typesafe.config.ConfigFactory
+
 object Main {
-  def main(args: Array[String]): Unit ={
-    println("Hello World!")
+
+  def main(args: Array[String]): Unit = {
+
+    val rootConfig = ConfigFactory.load()
+
+    val rootBehavior = Behaviors.setup[Nothing] { context =>
+      Behaviors.empty
+    }
+    val system = ActorSystem[Nothing](rootBehavior, "bank-system", rootConfig)
+    println(system.printTree)
   }
 }
